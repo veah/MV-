9099c05b50dad935ea97e086f71459fc3216e5fc
veah <391821744@qq.com>
2016-05-23 20:41:37 +0800

    add MVVM(with VUE)

---
+++ MVVM/mvvm.js
@@ -0,0 +1,23 @@
+var viewModel = require("vue");
+
+var app = new viewModel({
+	el:"#app",
+	data:{
+		newTodoContent:"",
+		todos:[{content:"make ppt!"},done:false]
+	},
+	methods:{
+		addNewTodo:function(){
+			if(!this.newTodoContent.length) return;
+			this.todos.push({
+				content:this.newTodoContent,
+				done:false
+			})
+			this.newTodoContent = "";
+		},
+		checkTodo:function(i){
+			var todo = this.todos[i];
+			todo.done = !todo.done
+		}
+	}
+})
